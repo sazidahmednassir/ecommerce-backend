efde708daf10f87419800e9b2b8595ca7c033a64
sazidahmednassir <sazidahmednassir@gmail.com>
2022-07-24 02:05:49 -0800

    add env

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express=require('express')
 const products =require('./data/products')
 const app=express()
+const dotenv = require('dotenv')
 
-console.log(products)
+const PORT=process.env.PORT || 5000;
+
+dotenv.config()
+
 
 app.get('/', (req, res)=>{
     res.send('Api is running')
+
 })
+
+app.get('/api/products', (req, res)=>{
+
+   
+    res.json(products)
+})
+
 app.get('/api/products/:id', (req, res)=>{
 
     const product= products.find(p=> p.id == req.params.id)
     res.json(product)
 })
 
-app.listen(5000, console.log('running on ecommerce server on 5000'))
+
+app.listen(PORT, ()=>{
+    console.log(`running on ecommerce server on ${PORT}`)
+})
